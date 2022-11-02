602057533ab9b9d7b96d916c3bcd93c3d3699c57
yopiangga <yopigambyok@gmail.com>
2022-11-02 14:11:19 +0700

    Minimum code

--- index.py
+++ index.py
@@ -1,17 +1,12 @@
-from typing import Type
-from urllib import response
-from flask_cors import CORS,cross_origin
-from flask import Flask,render_template, request
-import numpy as np
+from flask import Flask
 
 app = Flask(__name__)
-CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+
 
 @app.route('/')
-# @cross_origin()
 def home():
     return "home"
 
+
 if __name__ == '__main__':
-   app.run(debug=True)
+    app.run(debug=False)
