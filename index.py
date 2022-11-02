5a0ef196b790aef3f99411a2094fdc14dbdfc8f2
yopiangga <yopigambyok@gmail.com>
2022-11-02 14:13:51 +0700

    add route user

--- index.py
+++ index.py
@@ -5,7 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "home"
+    return "Selamat Datang di server Python Flask"
+
+
+@app.route('/users')
+def getUser():
+    return "Get User"
 
 
 if __name__ == '__main__':
